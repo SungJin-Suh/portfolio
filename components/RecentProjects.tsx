32ae36a6f8f3543e2b54fd42dea3fc618720dbc9
David Suh <ssuh10@my.bcit.ca>
2024-07-26 23:29:39 -0700

    Update project images with proper CSS formats

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -11,20 +11,19 @@ const RecentProjects = () => {
         <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
             {projects.map(({ id, title, des, img, iconLists, link }) => (
                 <div key={ id } className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
-                    <WobbleCard>
-                        <div className="relative flex flex-col items-center justify-center sm:w-96 w-[80vw] overflow-hidden">
+                    <WobbleCard containerClassName="h-[32.5rem]">
+                        <div className="relative flex flex-col items-center justify-center sm:w-96 w-[80vw] overflow-hidden pr-10">
                             <div className="text-center pr-10 pb-10 w-full">
                                 <h2 className="text-lg font-bold">{ title }</h2>
                                 <p className="text-sm mt-2">{ des }</p>
                             </div>
-                            <div>
-                                <img src="bg.png" alt="bg-img" />
-                            </div>
-                            <img 
+                            <div className="relative w-full h-full">
+                                <img 
                                 src={img}
                                 alt={title}
-                                className="z-10 absolute bottom-0"
+                                className="z-10 w-full object-cover h-full rounded-md rotate-2 scale-90"
                             />
+                            </div>
                         </div>
                     </WobbleCard>
                 </div>
